7322f2b24e75c9d4a1d05c129bd6f1d586a85d07
SakshiRastogi1302 <sakshi.rastogi1302@gmail.com>
2021-05-10 00:51:17 +0530

    Excel Sheet UI

--- activity/init.js
+++ activity/init.js
@@ -1,31 +1,31 @@
-let topRow=document.querySelector(".top_row");
-let leftColumn=document.querySelector(".left_column");
-let grid=document.querySelector(".grid");
+let topRow = document.querySelector(".top_row");
+let leftColumn = document.querySelector(".left_column");
+let gridBox = document.querySelector(".grid");
 
-let columns=26;
-for(let i=0;i<columns;i++){
-    let columnBox=document.createElement("div");
-    columnBox.setAttribute("class","cell");
-    columnBox.innerText=`${String.fromCharCode(65+i)}`;
+let columns = 26;
+for (let i = 0; i < columns; i++) {
+    let columnBox = document.createElement("div");
+    columnBox.setAttribute("class", "cell");
+    columnBox.innerText = `${String.fromCharCode(65+i)}`;
     topRow.appendChild(columnBox);
 }
 
-let rows=100;
-for(let i=0;i<rows;i++){
-    let rowBox=document.createElement("div");
-    rowBox.setAttribute("class","block");
-    rowBox.innerText=`${i}`;
+let rows = 100;
+for (let i = 0; i < rows; i++) {
+    let rowBox = document.createElement("div");
+    rowBox.setAttribute("class", "block");
+    rowBox.innerText = `${i}`;
     leftColumn.appendChild(rowBox);
 }
 
-for(let i=0;i<rows;i++){
-    let row=document.createElement("div");
-    row.setAttribute("class","row");
-    for(let j=0;j<columns;j++){
-        let col=document.createElement("div");
-        col.setAttribute("class","cell");
-        col.innerText=`${String.fromCharCode(65+j)} ${i+1}`;
+for (let i = 0; i < rows; i++) {
+    let row = document.createElement("div");
+    row.setAttribute("class", "row");
+    for (let j = 0; j < columns; j++) {
+        let col = document.createElement("div");
+        col.setAttribute("class", "cell");
+        col.innerText = `${String.fromCharCode(65+j)} ${i+1}`;
         row.append(col);
     }
-    grid.appendChild(row);
+    gridBox.appendChild(row);
 }
--- activity/script.js
+++ activity/script.js
@@ -1,41 +1,41 @@
-let iconContainer=document.querySelector(".icon_container");
-let sheetList=document.querySelector(".sheet_list");
-let firstSheet=document.querySelector(".sheet");
+let iconContainer = document.querySelector(".icon_container");
+let sheetList = document.querySelector(".sheet_list");
+let firstSheet = document.querySelector(".sheet");
 
-iconContainer.addEventListener("click",addSheets);
+iconContainer.addEventListener("click", addSheets);
 
-function addSheets(){
-    let newSheet=document.createElement("div");
-    let sheetArr=document.querySelectorAll(".sheet");
-    let lastSheet=sheetArr[sheetArr.length-1];
-    let lastIdx=lastSheet.getAttribute("sheet_idx");
-    lastIdx=Number(lastIdx);
-    newSheet.setAttribute("class","sheet");
-    newSheet.setAttribute("sheet_idx",`${lastIdx+1}`);
-    newSheet.innerText=`Sheet ${lastIdx+2}`;
+function addSheets() {
+    let newSheet = document.createElement("div");
+    let sheetArr = document.querySelectorAll(".sheet");
+    let lastSheet = sheetArr[sheetArr.length - 1];
+    let lastIdx = lastSheet.getAttribute("sheet_idx");
+    lastIdx = Number(lastIdx);
+    newSheet.setAttribute("class", "sheet");
+    newSheet.setAttribute("sheet_idx", `${lastIdx+1}`);
+    newSheet.innerText = `Sheet ${lastIdx+2}`;
     sheetList.appendChild(newSheet);
 
-    sheetArr=document.querySelectorAll(".sheet");
+    sheetArr = document.querySelectorAll(".sheet");
     makeLastSheetActive(sheetArr);
 
-    for(let i=0;i<sheetArr.length;i++){
-        sheetArr[i].addEventListener("click",makeSheetActiveOnClick);
+    for (let i = 0; i < sheetArr.length; i++) {
+        sheetArr[i].addEventListener("click", makeClickedSheetActive);
     }
 
 }
 
-function makeLastSheetActive(sheetArr){
-    for(let i=0;i<sheetArr.length;i++){
+function makeLastSheetActive(sheetArr) {
+    for (let i = 0; i < sheetArr.length; i++) {
         sheetArr[i].classList.remove("active_sheet");
     }
 
-    sheetArr[sheetArr.length-1].classList.add("active_sheet");
+    sheetArr[sheetArr.length - 1].classList.add("active_sheet");
 }
 
-function makeSheetActiveOnClick(e){
-    let currentSheet=e.currentTarget;
-    let sheetArr=document.querySelectorAll(".sheet");
-    for(let i=0;i<sheetArr.length;i++){
+function makeClickedSheetActive(e) {
+    let currentSheet = e.currentTarget;
+    let sheetArr = document.querySelectorAll(".sheet");
+    for (let i = 0; i < sheetArr.length; i++) {
         sheetArr[i].classList.remove("active_sheet");
     }
 
