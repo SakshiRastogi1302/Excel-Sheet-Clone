e77d1ff880488e8baa553984200551d2a6fbde04
SakshiRastogi1302 <sakshi.rastogi1302@gmail.com>
2021-05-15 02:07:25 +0530

    styling features added

--- activity/init.js
+++ activity/init.js
@@ -2,21 +2,27 @@ let topRow=document.querySelector(".top_row");
 let leftColumn=document.querySelector(".left_column");
 let gridBox=document.querySelector(".grid");
 
+// ****************************************** TOP ROW ******************************************************
+
 let columns=26;
 for(let i=0;i<columns;i++){
     let columnBox=document.createElement("div");
-    columnBox.setAttribute("class","cell");
+    columnBox.setAttribute("class","top_row_cell");
     columnBox.innerText=`${String.fromCharCode(65+i)}`;
     topRow.appendChild(columnBox);
 }
+
+// ***************************************** LEFT COLUMN *****************************************************
 
 let rows=100;
 for(let i=0;i<rows;i++){
     let rowBox=document.createElement("div");
     rowBox.setAttribute("class","block");
-    rowBox.innerText=`${i}`;
+    rowBox.innerText=`${i+1}`;
     leftColumn.appendChild(rowBox);
 }
+
+// **************************************** GRID BOX *********************************************************
 
 for(let i=0;i<rows;i++){
     let row=document.createElement("div");
@@ -24,7 +30,10 @@ for(let i=0;i<rows;i++){
     for(let j=0;j<columns;j++){
         let col=document.createElement("div");
         col.setAttribute("class","cell");
-        col.innerText=`${String.fromCharCode(65+j)} ${i+1}`;
+        col.setAttribute("rid",`${i}`);
+        col.setAttribute("cid",`${j}`);
+        col.setAttribute("contenteditable","true");
+        // col.innerText=`${String.fromCharCode(65+j)} ${i+1}`;
         row.append(col);
     }
     gridBox.appendChild(row);
--- activity/script.js
+++ activity/script.js
@@ -1,6 +1,13 @@
 let iconContainer=document.querySelector(".icon_container");
 let sheetList=document.querySelector(".sheet_list");
 let firstSheet=document.querySelector(".sheet");
+let allCells=document.querySelectorAll(".grid .cell");
+let addressBox=document.querySelector(".address_box");
+let leftAlignment=document.querySelector(".left_alignment_button");
+let rightAlignment=document.querySelector(".right_alignment_button");
+let centerAlignment=document.querySelector(".center_alignment_button");
+let fontButton=document.querySelector(".font_size");
+
 
 iconContainer.addEventListener("click",addSheets);
 
@@ -41,3 +48,65 @@ function makeSheetActiveOnClick(e){
 
     currentSheet.classList.add("active_sheet");
 }
+
+
+for(let i=0;i<allCells.length;i++){
+    allCells[0].click(); //On opening first cell should always be clicked
+    allCells[i].addEventListener("click",handleCell);
+}
+
+function handleCell(e){
+    let cell=e.currentTarget;
+    let rid=Number(cell.getAttribute("rid"));
+    let cid=Number(cell.getAttribute("cid"));
+
+    let rowAddress=rid+1;
+    let columnAddress=String.fromCharCode(cid+65);
+
+    let cellAddress=columnAddress+rowAddress;
+    addressBox.value=cellAddress;
+
+}
+
+
+leftAlignment.addEventListener("click",function(){
+    let address=addressBox.value;
+    let {rid,cid}= convertIntoIndexes(address);
+    console.log(rid,cid);
+    let cell=document.querySelector(`.cell[rid="${rid}"][cid="${cid}"]`);
+    cell.style.textAlign="left";
+});
+
+rightAlignment.addEventListener("click",function(){
+    let address=addressBox.value;
+    let {rid,cid}= convertIntoIndexes(address);
+    console.log(rid,cid);
+    let cell=document.querySelector(`.cell[rid="${rid}"][cid="${cid}"]`);
+    cell.style.textAlign="right";
+});
+
+centerAlignment.addEventListener("click",function(){
+    let address=addressBox.value;
+    let {rid,cid}= convertIntoIndexes(address);
+    console.log(rid,cid);
+    let cell=document.querySelector(`.cell[rid="${rid}"][cid="${cid}"]`);
+    cell.style.textAlign="center";
+});
+
+function convertIntoIndexes(address){
+    let columnAddress=address.charCodeAt(0);
+    let cid=columnAddress-65;
+    let rowAddress=address.slice(1);
+    let rid=rowAddress-1;
+    return {cid,rid};
+}
+
+fontButton.addEventListener("change",function(){
+    let fontSize=fontButton.value;
+    console.log(fontSize);
+    let address=addressBox.value;
+    let {rid,cid}= convertIntoIndexes(address);
+    console.log(rid,cid);
+    let cell=document.querySelector(`.cell[rid="${rid}"][cid="${cid}"]`);
+    cell.style.fontSize=fontSize+"px";
+});
